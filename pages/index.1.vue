c45f71dec8f2978f914d010bfbc74a3dbf760110
limjuchan <sa0466@nate.com>
2018-06-19 14:23:22 +0900

    first commit

--- pages/index.1.vue
+++ pages/index.1.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="hero is-primary">
+  <section class="hero is-light">
   <div class="hero-body">
     <div class="container">
       <h1 class="title">
@@ -12,27 +12,46 @@
   <div class="tile is-vertical is-8">
     <div class="tile">
       <div class="tile is-parent is-vertical">
-        <article class="tile is-child notification is-primary">
+        <article class="tile is-child notification is-Dark">
           <p class="title">NAME</p>
           <p class="subtitle">박태건</p>
         </article>
-        <article class="tile is-child notification is-primary">
+        <article class="tile is-child notification is-Dark">
           <p class="title">H.P</p>
           <p class="subtitle">010-7175-3348</p>
         </article>
       </div>
       <div class="tile is-parent">
-        <article class="tile is-child notification is-primary">
+        <article class="tile is-child notification is-Dark">
           <p class="title">PHOTO</p>
           <p class="subtitle"></p>
           <figure class="image is-256x128">
-            <img src="https://bulma.io/images/placeholders/640x480.png">
+            <img src="~static/내사진.jpg">
           </figure>
+          <table class="table">
+  <thead>
+    <tr>
+      <th><abbr title="Position">NAME</abbr></th>
+      <th><abbr title="Position">AGE</abbr></th>
+      <th><abbr title="Position">HOBBY</abbr></th>
+    </tr>
+  </thead>
+  
+  <tbody>
+    <tr>
+      <th><abbr title="Position">박태건</abbr></th>
+      <th><abbr title="Position">25</abbr></th>
+      <th><abbr title="Position">음악듣기</abbr></th>
+    </tr>
+  
+  </tbody>
+</table>
         </article>
       </div>
     </div>
+    
     <div class="tile is-parent">
-      <article class="tile is-child notification is-primary">
+      <article class="tile is-child notification is-Dark">
         <p class="title">E-mail</p>
         <p class="subtitle">qkrxo876@hanmail.net</p>
         <div class="content">
@@ -42,13 +61,15 @@
     </div>
   </div>
   <div class="tile is-parent">
-    <article class="tile is-child notification is-success">
+    <article class="tile is-child notification is-Dark">
       <div class="content">
         <p class="title">DEPARTMENT</p>
         <p class="subtitle">정보통신융합공학과</p>
         <figure class="image is-512x512">
             <img src="~static/정보통신공학과.png">
           </figure>
+          <p class="subtitle">정보통신 서비스가 고품질화되고 멀티미디어화 됨에 따라 이를 실현하는 정보통신 기술이 눈부시게 발전하고 있습니다. 국내외적으로 스마트폰과 태블릿과 같은 스마트 디바이스 기술과 이를 이용한 모바일 정보통신 소프트웨어 및 모바일 프로그래밍 기술은 고부가가치의 전략 품목으로 인식되고 있습니다. 따라서 정보통신 기술(ICT) 기반의 시대적 요구가 증대되고 있는 정보통신 기술의 융합과 스마트 모바일 기술을 특성화하여 졸업생들의 엔지니어적 경쟁력을 지속적으로 강화하고 있습니다. 현재 사회의 화두가 되는 “4차 산업혁명”의 기반 기술은 정보통신 기술(ICT)의 융합이므로 정보통신 기술과 스마트 모바일 기술을 창의적으로 연구개발하여 새로운 부가가치를 창조할 수 있습니다.</p>
+          
         <div class="content">
           <!-- Content -->
         </div>
@@ -57,6 +78,19 @@
   </div>
 </div>
     </div>
+     <footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>park</strong> by <a href="https://jgthms.com">Taegun</a>. address. gyeong-gidoui jeongbusi howondong sin-won A d 101동 1701 호
+        <a href="http://opensource.org/licenses/mit-license.php">park</a>. 
+         <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"> BY 2018-06-18</a>.
+      </p>
+    </div>
   </div>
+</footer>
+  </div>
+
 </section>
+
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -60,3 +60,99 @@
   </div>
 </section>
 </template>
+<template>
+  <section class="hero is-light">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        PORTFOLIO
+      </h1>
+      <h2 class="subtitle">
+         SELF-INTRODUCTION
+      </h2>
+      <div class="tile is-ancestor">
+  <div class="tile is-vertical is-8">
+    <div class="tile">
+      <div class="tile is-parent is-vertical">
+        <article class="tile is-child notification is-Dark">
+          <p class="title">NAME</p>
+          <p class="subtitle">박태건</p>
+        </article>
+        <article class="tile is-child notification is-Dark">
+          <p class="title">H.P</p>
+          <p class="subtitle">010-7175-3348</p>
+        </article>
+      </div>
+      <div class="tile is-parent">
+        <article class="tile is-child notification is-Dark">
+          <p class="title">PHOTO</p>
+          <p class="subtitle"></p>
+          <figure class="image is-256x128">
+            <img src="~static/내사진.jpg">
+          </figure>
+          <table class="table">
+  <thead>
+    <tr>
+      <th><abbr title="Position">NAME</abbr></th>
+      <th><abbr title="Position">AGE</abbr></th>
+      <th><abbr title="Position">HOBBY</abbr></th>
+    </tr>
+  </thead>
+  
+  <tbody>
+    <tr>
+      <th><abbr title="Position">박태건</abbr></th>
+      <th><abbr title="Position">25</abbr></th>
+      <th><abbr title="Position">음악듣기</abbr></th>
+    </tr>
+  
+  </tbody>
+</table>
+        </article>
+      </div>
+    </div>
+    
+    <div class="tile is-parent">
+      <article class="tile is-child notification is-Dark">
+        <p class="title">E-mail</p>
+        <p class="subtitle">qkrxo876@hanmail.net</p>
+        <div class="content">
+          <!-- Content -->
+        </div>
+      </article>
+    </div>
+  </div>
+  <div class="tile is-parent">
+    <article class="tile is-child notification is-Dark">
+      <div class="content">
+        <p class="title">DEPARTMENT</p>
+        <p class="subtitle">정보통신융합공학과</p>
+        <figure class="image is-512x512">
+            <img src="~static/정보통신공학과.png">
+          </figure>
+          <p class="subtitle">정보통신 서비스가 고품질화되고 멀티미디어화 됨에 따라 이를 실현하는 정보통신 기술이 눈부시게 발전하고 있습니다. 국내외적으로 스마트폰과 태블릿과 같은 스마트 디바이스 기술과 이를 이용한 모바일 정보통신 소프트웨어 및 모바일 프로그래밍 기술은 고부가가치의 전략 품목으로 인식되고 있습니다. 따라서 정보통신 기술(ICT) 기반의 시대적 요구가 증대되고 있는 정보통신 기술의 융합과 스마트 모바일 기술을 특성화하여 졸업생들의 엔지니어적 경쟁력을 지속적으로 강화하고 있습니다. 현재 사회의 화두가 되는 “4차 산업혁명”의 기반 기술은 정보통신 기술(ICT)의 융합이므로 정보통신 기술과 스마트 모바일 기술을 창의적으로 연구개발하여 새로운 부가가치를 창조할 수 있습니다.</p>
+          
+        <div class="content">
+          <!-- Content -->
+        </div>
+      </div>
+    </article>
+  </div>
+</div>
+    </div>
+     <footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>park</strong> by <a href="https://jgthms.com">Taegun</a>. address. gyeong-gidoui jeongbusi howondong sin-won A d 101동 1701 호
+        <a href="http://opensource.org/licenses/mit-license.php">park</a>. 
+         <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"> BY 2018-06-18</a>.
+      </p>
+    </div>
+  </div>
+</footer>
+  </div>
+
+</section>
+
+</template>
